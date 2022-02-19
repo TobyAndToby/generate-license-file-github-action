efe6f66c01638362b5b19f75e0cb491c07298475
Toby Smith <tobysmith568@hotmail.co.uk>
2022-02-19 22:45:56 +0000

    Add a basic implementation

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { getInput, error } from "@actions/core";
+import { generateLicenseFile, isValidEol } from "generate-license-file/dist/generateLicenseFile";
+
+const main = async () => {
+  try {
+    await runGitHubAction();
+  } catch (error: unknown) {
+    handleErrorInGitHubAction(error);
+  }
+};
+
+const runGitHubAction = async () => {
+  const packageJsonLocation = getInput("input");
+  const licenseFileLocation = getInput("output");
+  const lineEnding = getInput("lineEnding");
+
+  const validEol = isValidEol(lineEnding);
+  if (!validEol) {
+    error("The given line ending is not valid");
+    return;
+  }
+
+  await generateLicenseFile(packageJsonLocation, licenseFileLocation, lineEnding);
+};
+
+const handleErrorInGitHubAction = (thrown: unknown) => {
+  if (thrown instanceof Error) {
+    error(thrown.message);
+    return;
+  }
+
+  if (typeof thrown === "string") {
+    error(thrown);
+    return;
+  }
+
+  error("Unknown error in Generate-License-File");
+};
+
+(async () => {
+  await main();
+})();
