a7c0d006c31db4283cc8282bafd879e5e519565f
Toby Smith <tobysmith568@hotmail.co.uk>
2022-02-20 15:55:28 +0000

    Fix e2e tests

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { getInput, setFailed } from "@actions/core";
+import { getInput, info, setFailed } from "@actions/core";
 import { generateLicenseFile, LineEnding } from "generate-license-file";
 import { isValidEol } from "generate-license-file/dist/generateLicenseFile";
 
@@ -14,6 +14,10 @@ const runGitHubAction = async () => {
   const packageJsonLocationInput = getInput("input", { required: true });
   const licenseFileLocationInput = getInput("output", { required: true });
   const lineEndingInput = getInput("lineEnding");
+
+  info(`Using package.json location: ${packageJsonLocationInput}`);
+  info(`Using license file location: ${licenseFileLocationInput}`);
+  info(`Using line ending: ${lineEndingInput}`);
 
   const lineEnding = parseLineEnding(lineEndingInput);
 
